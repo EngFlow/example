32f8bd8709827f06d4bb6801968b89a2a6869321
Andrés Felipe Barco Santa <andres@engflow.com>
2022-08-12 14:10:22 -0500

    Code formated and exception added to channel creation

--- java/com/engflow/notificationqueue/Client.java
+++ java/com/engflow/notificationqueue/Client.java
@@ -5,35 +5,28 @@ import com.engflow.eventstore.v1.EventStoreGrpc;
 import com.engflow.eventstore.v1.GetInvocationRequest;
 import com.engflow.eventstore.v1.StreamedBuildEvent;
 import com.engflow.notification.v1.Notification;
-import com.engflow.notification.v1.PullNotificationResponse;
-import com.engflow.notification.v1.PullNotificationRequest;
 import com.engflow.notification.v1.NotificationQueueGrpc;
+import com.engflow.notification.v1.PullNotificationRequest;
+import com.engflow.notification.v1.PullNotificationResponse;
 import com.engflow.notification.v1.QueueId;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
-
 import io.grpc.ManagedChannel;
 import io.grpc.Metadata;
-import io.grpc.Context;
-import io.grpc.stub.MetadataUtils;
 import io.grpc.netty.GrpcSslContexts;
 import io.grpc.netty.NegotiationType;
 import io.grpc.netty.NettyChannelBuilder;
-import io.netty.handler.ssl.SslContextBuilder;
+import io.grpc.stub.MetadataUtils;
 import io.grpc.stub.StreamObserver;
-
-
-import javax.annotation.Nullable;
-import javax.net.ssl.SSLException;
+import io.netty.handler.ssl.SslContextBuilder;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import org.apache.commons.cli.Options;
+import javax.annotation.Nullable;
+import javax.net.ssl.SSLException;
 
 class Client {
 
@@ -56,10 +49,10 @@ class Client {
     ManagedChannel channel = null;
     try {
       channel =
-              createChannel(
-                      clientOptions.getOption("notification_queue_endpoint"),
-                      clientOptions.getOption("tls_certificate"),
-                      clientOptions.getOption("tls_key"));
+          createChannel(
+              clientOptions.getOption("notification_queue_endpoint"),
+              clientOptions.getOption("tls_certificate"),
+              clientOptions.getOption("tls_key"));
 
     } catch (IllegalArgumentException e) {
       System.err.println("Unable to open channel to " + args[0].split("=")[1]);
@@ -67,10 +60,13 @@ class Client {
     } catch (IllegalStateException e) {
       System.err.println("Unable to open channel to " + args[0].split("=")[1]);
       throw new IllegalStateException(e);
+    } catch (IOException e) {
+      throw new IOException(e);
     }
     try {
       final Metadata header = new Metadata();
-      Metadata.Key<String> userKey = Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER);
+      Metadata.Key<String> userKey =
+          Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER);
       header.put(userKey, "Bearer " + clientOptions.getOption("token"));
       pull(channel, clientOptions.getOption("queue_name"), header);
     } finally {
@@ -85,7 +81,17 @@ class Client {
     }
   }
 
-  private static void pull(ManagedChannel channel, String queueName, Metadata header) throws InterruptedException {
+  /**
+   * Gets the notification streams from a {@link NotificationQueueGrpc} stub and then calls
+   * getInvocation for each received notification
+   *
+   * @param channel a grpc channel for cluster connection
+   * @param queueName name of the queue to listen to
+   * @param header metadata for token authentication (if needed)
+   * @throws InterruptedException
+   */
+  private static void pull(ManagedChannel channel, String queueName, Metadata header)
+      throws InterruptedException {
     NotificationQueueGrpc.NotificationQueueStub asyncStub = NotificationQueueGrpc.newStub(channel);
     asyncStub = MetadataUtils.attachHeaders(asyncStub, header);
     final CountDownLatch finishLatch = new CountDownLatch(1);
@@ -100,10 +106,17 @@ class Client {
                 try {
                   BuildLifecycleEventNotification lifeCycleEvent =
                       notificationContent.unpack(BuildLifecycleEventNotification.class);
-                  // Check if this is an invocation started event
+                  /**
+                   * Check if this is an invocation started event. Options are INVOCATION_STARTED
+                   * and INVOCATION_FINISHED
+                   */
                   if (lifeCycleEvent.getKindCase().name().equals("INVOCATION_STARTED")) {
                     String invocation = lifeCycleEvent.getInvocationStarted().getInvocationId();
                     try {
+                      /**
+                       * Fetch the invocation using the grpc {@link EventStoreGrpc} stub using the
+                       * acquired invocation id
+                       */
                       getInvocations(channel, invocation, header);
                     } catch (InterruptedException e) {
                       System.err.println("Could not get invocation with uuid " + invocation);
@@ -142,6 +155,14 @@ class Client {
     finishLatch.await();
   }
 
+  /**
+   * Gets an invocation from the {@link EventStoreGrpc} stub
+   *
+   * @param channel a grpc channel for cluster connection
+   * @param invocationId the id of the required notification
+   * @param header metadata for token authentication (if needed)
+   * @throws InterruptedException
+   */
   public static void getInvocations(ManagedChannel channel, String invocationId, Metadata header)
       throws InterruptedException {
     EventStoreGrpc.EventStoreStub asyncStub = EventStoreGrpc.newStub(channel);
@@ -167,9 +188,8 @@ class Client {
   }
 
   private static ManagedChannel createChannel(
-      String endpoint,
-      @Nullable String clientCertificate,
-      @Nullable String clientKey) {
+      String endpoint, @Nullable String clientCertificate, @Nullable String clientKey)
+      throws IOException {
     Preconditions.checkArgument(
         !Strings.isNullOrEmpty(endpoint),
         "Empty --notification_queue_endpoint, expected \"protocol://host[:port]\"");
@@ -186,7 +206,6 @@ class Client {
         NettyChannelBuilder.forTarget(endpoint)
             .negotiationType(tls ? NegotiationType.TLS : NegotiationType.PLAINTEXT);
 
-
     if (tls) {
       try {
         SslContextBuilder contextBuilder = GrpcSslContexts.forClient();
@@ -197,10 +216,10 @@ class Client {
         builder.sslContext(contextBuilder.build());
       } catch (SSLException e) {
         throw new IllegalStateException(e);
+      } catch (IOException e) {
+        throw new IOException(e);
       }
     }
     return builder.build();
   }
-
-
 }
--- java/com/engflow/notificationqueue/NotificationOptions.java
+++ java/com/engflow/notificationqueue/NotificationOptions.java
@@ -1,89 +1,95 @@
 package com.engflow.notificationqueue;
 
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.OptionBuilder;
-import org.apache.commons.cli.Option.Builder;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public class NotificationOptions {
-    Options options = new Options();
-    CommandLineParser parser = new DefaultParser();
-    CommandLine cmd;
-    public NotificationOptions(){}
+  Options options = new Options();
+  CommandLineParser parser = new DefaultParser();
+  CommandLine cmd;
 
-    public NotificationOptions parseOptions(String[] args) {
-        instantiateOptions();
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            throw new IllegalArgumentException(e);
-        }
+  public NotificationOptions() {}
 
-        if (cmd.hasOption("queue_name") && cmd.hasOption("notification_queue_endpoint")){
-            return this;
-        } else {
-            throw new IllegalArgumentException("Please provide the arguments...");
-        }
+  public NotificationOptions parseOptions(String[] args) {
+    instantiateOptions();
+    try {
+      cmd = parser.parse(options, args);
+    } catch (ParseException e) {
+      throw new IllegalArgumentException(e);
     }
 
-    public String getOption(String value){
-        return cmd.getOptionValue(value);
+    if (cmd.hasOption("queue_name") && cmd.hasOption("notification_queue_endpoint")) {
+      return this;
+    } else {
+      throw new IllegalArgumentException("Please provide the arguments...");
     }
+  }
 
-    private void instantiateOptions(){
-        Option queueName = Option.builder()
-                .longOpt("queue_name")
-                .argName("property=value" )
-                .hasArgs()
-                .valueSeparator()
-                .numberOfArgs(2)
-                .desc("The name of the queue that you want to receive notifications from." )
-                .build();
-        options.addOption(queueName);
+  public String getOption(String value) {
+    return cmd.getOptionValue(value);
+  }
 
-        Option endpoint = Option.builder()
-                .longOpt("notification_queue_endpoint")
-                .argName("property=value" )
-                .hasArgs()
-                .valueSeparator()
-                .numberOfArgs(2)
-                .desc("The service endpoint in protocol://host:port format. The protocol must be 'grpc' or "
-                        + "'grpcs' signifying plaintext or TLS-encrypted communication respectively.")
-                .build();
-        options.addOption(endpoint);
+  private void instantiateOptions() {
+    Option queueName =
+        Option.builder()
+            .longOpt("queue_name")
+            .argName("property=value")
+            .hasArgs()
+            .valueSeparator()
+            .numberOfArgs(2)
+            .desc("The name of the queue that you want to receive notifications from.")
+            .build();
+    options.addOption(queueName);
 
-        Option certificate = Option.builder()
-                .longOpt("tls_certificate")
-                .argName("property=value" )
-                .hasArgs()
-                .valueSeparator()
-                .numberOfArgs(2)
-                .desc("The path of the mTLS certificate to use as the client certificate." )
-                .build();
-        options.addOption(certificate);
+    Option endpoint =
+        Option.builder()
+            .longOpt("notification_queue_endpoint")
+            .argName("property=value")
+            .hasArgs()
+            .valueSeparator()
+            .numberOfArgs(2)
+            .desc(
+                "The service endpoint in protocol://host:port format. The protocol must be 'grpc'"
+                    + " or 'grpcs' signifying plaintext or TLS-encrypted communication"
+                    + " respectively.")
+            .build();
+    options.addOption(endpoint);
 
-        Option key = Option.builder()
-                .longOpt("tls_key")
-                .argName("property=value" )
-                .hasArgs()
-                .valueSeparator()
-                .numberOfArgs(2)
-                .desc("Path to the `--tls_certificate`'s private key." )
-                .build();
-        options.addOption(key);
+    Option certificate =
+        Option.builder()
+            .longOpt("tls_certificate")
+            .argName("property=value")
+            .hasArgs()
+            .valueSeparator()
+            .numberOfArgs(2)
+            .desc("The path of the mTLS certificate to use as the client certificate.")
+            .build();
+    options.addOption(certificate);
 
-        Option remoteHeader = Option.builder()
-                .longOpt("token")
-                .argName("property=value" )
-                .hasArgs()
-                .valueSeparator()
-                .numberOfArgs(2)
-                .desc("Token for open access clusters." )
-                .build();
-        options.addOption(remoteHeader);
-    }
+    Option key =
+        Option.builder()
+            .longOpt("tls_key")
+            .argName("property=value")
+            .hasArgs()
+            .valueSeparator()
+            .numberOfArgs(2)
+            .desc("Path to the `--tls_certificate`'s private key.")
+            .build();
+    options.addOption(key);
+
+    Option remoteHeader =
+        Option.builder()
+            .longOpt("token")
+            .argName("property=value")
+            .hasArgs()
+            .valueSeparator()
+            .numberOfArgs(2)
+            .desc("Token for open access clusters.")
+            .build();
+    options.addOption(remoteHeader);
+  }
 }
