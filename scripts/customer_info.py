21e9ed69bf0beae65bedcd50cb9950e55ce7a3ce
Sarah Raza <sarahraza007@stanford.edu>
2022-11-21 13:42:26 -0800

    removed comments, added command

--- scripts/customer_info.py
+++ scripts/customer_info.py
@@ -1,11 +1,13 @@
 import os
+import sys
+
 import yaml
 
-# change this based on customer repo path
-repo_path = "/"
+# to run cd into scripts and run command "python customer_info.py PATH" where path = absolute path to yaml file
+# EX: python customer_info.py "/Users/sarahraza/example/scripts/"
 
 # change path for targets as relevant, relative to repo
-bazel_target_command = "bazel cquery //java/..."
+bazel_cquery_target_command = "bazel cquery //java/..."
 
 # change path for actions as relevant, relative to repo
 bazel_action_command = "bazel aquery //java/..."
@@ -13,7 +15,7 @@ bazel_action_command = "bazel aquery //java/..."
 
 def writeToFile(dict_file):
     print(dict_file)
-    with open(r'scripts/customer_info.yaml', 'w') as file:
+    with open(r'customer_info.yaml', 'w') as file:
         yaml.dump(dict_file, file)
 
 
@@ -22,19 +24,14 @@ if __name__ == '__main__':
     dict_file = {}
 
     # move to customer project
-    os.chdir(repo_path)
+    os.chdir(sys.argv[1])
 
     # get bazel version
     bazel_version = os.popen("bazel --version").read()
-    print("bazel_version var", bazel_version)
-    dict_file["bazel version"] = bazel_version.strip()
+    dict_file["bazel version"] = str(bazel_version.strip())
 
     # get targets
-    # bazel_target = os.popen(bazel_target_command).read()
-    # print("bazel_target var", bazel_target)
-
-    # get actions
-    # bazel_action = os.popen(bazel_action_command).read()
-    # print("bazel_target var", bazel_action)
+    bazel_target = os.popen(bazel_cquery_target_command).read()
+    dict_file["bazel cquery targets"] = bazel_target
 
     writeToFile(dict_file)
