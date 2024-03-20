341284d6e584bca566157be68f5182303bcce764
Andrés Felipe Barco <andrez.fbs@gmail.com>
2024-03-20 11:23:27 -0500

    [windows] Enabling dev mode in docker

--- platform/windows_x64/docker/normalize_os_windows.ps1
+++ platform/windows_x64/docker/normalize_os_windows.ps1
@@ -12,3 +12,11 @@ Set-StrictMode -Version latest
 # This directory is removed below after reboot.
 New-Item -Type Directory -Path "${env:EF_INSTALL_TEMP_DIR}"
 
+# Enable developer mode. This is needed so that the worker service can create symbolic links.
+# Adapted from https://learn.microsoft.com/en-us/windows/apps/get-started/developer-mode-features-and-debugging#use-powershell-to-enable-your-device
+New-ItemProperty `
+    -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" `
+    -Name "AllowDevelopmentWithoutDevLicense" `
+    -Value 1 `
+    -PropertyType DWORD `
+    -Force
