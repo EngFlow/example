494f4580e0292418b2515c85f036e31f48bcfea5
Nicolas Lopez <nlopezgi@gmail.com>
2024-11-22 15:35:56 -0500

    fixing building zstd

--- infra/setup-buck2.sh
+++ infra/setup-buck2.sh
@@ -3,17 +3,16 @@ set -x
 
 # Build zstd: Remove once zstd is insalled in CI runner.
 git clone https://github.com/facebook/zstd.git
-cd zstd/build/cmake
-cmake .
+cd zstd/
 make
-cd ../../../
+cd ../
 
 # Get the Buck2 binary.
-wget https://github.com/facebook/buck2/releases/download/latest/buck2-aarch64-unknown-linux-gnu.zst
+curl --output buck2-aarch64-unknown-linux-gnu.zst https://github.com/facebook/buck2/releases/download/latest/buck2-aarch64-unknown-linux-gnu.zst
 
 # Unpack the binary.
 # Use installed zstd once available in the CI runner.
-zstd/build/cmake/programs/zstd -d buck2-aarch64-unknown-linux-gnu.zst
+./zstd/zstd -d buck2-aarch64-unknown-linux-gnu.zst
 # unzstd buck2-aarch64-unknown-linux-gnu.zst
 
 # Change its name and make it executable.
