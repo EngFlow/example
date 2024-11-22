1cce4856c342ef241a6a9ea661c4742c02c633da
Nicolas Lopez <nlopezgi@gmail.com>
2024-11-22 17:33:41 -0500

    fix auth header

--- infra/auth-buck2.sh
+++ infra/auth-buck2.sh
@@ -5,5 +5,5 @@ set -x
 find ./ -type f -name '.buckconfig' -exec sed -i 's/<CLUSTER_NAME>/opal/g' {} \;
 
 # Modify the buck2/**/.buckconfig files to set the "Authorization=Bearer $GITHUB_TOKEN" in the http_headers.
-find ./ -type f -name '.buckconfig' -exec sed -i 's/x-engflow-auth-method:jwt-v0,x-engflow-auth-token:LONG_JWT_STRING/Authorization=Bearer $GITHUB_TOKEN/g' {} \;
+find ./ -type f -name '.buckconfig' -exec sed -i 's/x-engflow-auth-method:jwt-v0,x-engflow-auth-token:LONG_JWT_STRING/Authorization:Bearer $GITHUB_TOKEN/g' {} \;
 
