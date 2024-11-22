1043d3355d1aef6f07e217ac1b61eee9e7f1f64f
Nicolas Lopez <nlopezgi@gmail.com>
2024-11-22 14:57:09 -0500

    fix find command

--- infra/auth-buck2.sh
+++ infra/auth-buck2.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Modify the buck2/**/.buckconfig files to set the test cluster to opal.
-find . -type f -name ".buckconfig" -exec sed -i 's/<CLUSTER_NAME>/opal/' {} \;
+find ./ -type f -name '.buckconfig' -exec sed -i 's/<CLUSTER_NAME>/opal/g' {} \;
 
 # Modify the buck2/**/.buckconfig files to set the "Authorization=Bearer $GITHUB_TOKEN" in the http_headers.
-find . -type f -name ".buckconfig" sed -i 's/x-engflow-auth-method:jwt-v0,x-engflow-auth-token:LONG_JWT_STRING/Authorization=Bearer $GITHUB_TOKEN/' {} \;
+find ./ -type f -name '.buckconfig' -exec sed -i 's/x-engflow-auth-method:jwt-v0,x-engflow-auth-token:LONG_JWT_STRING/Authorization=Bearer $GITHUB_TOKEN/g' {} \;
 
 # Testing only - remove before merging
 cat buck2/cpp/.buckconfig
--- infra/test-buck2.sh
+++ infra/test-buck2.sh
@@ -2,18 +2,19 @@
 
 # Run cpp example
 cd buck2/cpp
-../../buck2 build //:cpp_lib
-../../buck2 test //:cpp_test
+../../buck2-exe build //:cpp_lib
+../../buck2-exe test //:cpp_test
 cd ..
 
 # Run python example
 cd python
-../../buck2 build //main:check_main
-../../buck2 test //hello:hello_unittest_test
+../../buck2-exe build //main:check_main
+../../buck2-exe test //hello:hello_unittest_test
 cd ..
 
 # Run go example
+pwd
 cd go
-../../buck2 build //go:hello
-../../buck2 test //go/greeting:greeting_test
+../../buck2-exe build //go:hello
+../../buck2-exe test //go/greeting:greeting_test
 cd ..
