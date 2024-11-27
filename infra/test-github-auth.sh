97253f522d644e1699d77df375520bbe9fa0b8b3
Nicolas Lopez <nlopezgi@gmail.com>
2024-11-27 11:37:14 -0500

    test using github auth for EF CI

--- infra/test-github-auth.sh
+++ infra/test-github-auth.sh
@@ -2,5 +2,5 @@
 set -x
 
 touch .bazelrc.user
-echo 'build:opal_auth -- remote_header=\"Authorization=Bearer $GITHUB_TOKEN\"' >> .bazelrc.user
+echo 'build:opal_auth --remote_header=\"Authorization=Bearer $GITHUB_TOKEN\"' >> .bazelrc.user
 cat .bazelrc.user
