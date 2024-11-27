e8bf5160309e1bc68592c41b8c80dc58b8c4d1e6
Nicolas Lopez <nlopezgi@gmail.com>
2024-11-27 11:59:51 -0500

    test acces to container

--- infra/test-github-auth.sh
+++ infra/test-github-auth.sh
@@ -4,3 +4,7 @@ set -x
 touch .bazelrc.user
 echo 'build:opal_auth --remote_header=\"Authorization=Bearer $GITHUB_TOKEN\"' >> .bazelrc.user
 cat .bazelrc.user
+
+
+echo "$GITHUB_TOKEN" | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
+docker inspect ghcr.io/engflow/hello-world:1.0
