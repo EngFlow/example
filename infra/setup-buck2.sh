3d19f29c46f0251c042a96f02ef3530f7eb2b668
Nicolas Lopez <nlopezgi@gmail.com>
2024-12-02 13:20:11 -0500

    use installed zstd

--- infra/setup-buck2.sh
+++ infra/setup-buck2.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 set -xe
 
-# Build zstd: Remove once zstd is insalled in CI runner.
-git clone https://github.com/facebook/zstd.git
-cd zstd/
-make
-cd ..
-
 # Get the Buck2 binary.
 curl -L -O https://github.com/facebook/buck2/releases/download/latest/buck2-x86_64-unknown-linux-musl.zst
 
-
-
 # Unpack the binary.
-# Use installed zstd once available in the CI runner.
-./zstd/zstd -d buck2-x86_64-unknown-linux-musl.zst
-# unzstd buck2-x86_64-unknown-linux-musl.zst
+unzstd buck2-x86_64-unknown-linux-musl.zst
 
 # Change its name and make it executable.
 mv buck2-x86_64-unknown-linux-musl buck2-exe
