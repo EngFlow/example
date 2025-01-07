66fd0f403f6c670a2aa863812035419df864a50c
Nicolas Lopez <nlopezgi@gmail.com>
2025-01-07 13:21:13 -0500

    use --remote_only

--- infra/test-buck2.sh
+++ infra/test-buck2.sh
@@ -6,20 +6,20 @@ alias buck2="$(realpath buck2-exe)"
 
 # Run cpp example
 cd buck2/cpp
-buck2 build --target-platforms //platforms:remote_platform //:cpp_lib
-buck2 test --target-platforms //platforms:remote_platform //:cpp_test
+buck2 build --remote-only //:cpp_lib
+buck2 test --remote-only //:cpp_test
 cd ..
 
 # Run python example
 cd python
-buck2 build --target-platforms //platforms:remote_platform //main:check_main
-buck2 test --target-platforms //platforms:remote_platform //hello:hello_unittest_test
+buck2 build --remote-only//main:check_main
+buck2 test ---remote-only //hello:hello_unittest_test
 cd ..
 
 # Run go example
 cd golang
-buck2 build --target-platforms //platforms:remote_platform //go:hello
-buck2 test --target-platforms //platforms:remote_platform //go/greeting:greeting_test
+buck2 build --remote-only //go:hello
+buck2 test --remote-only //go/greeting:greeting_test
 cd ..
 
 # Run rust example
