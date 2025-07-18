8b2893ca89210dd2193e8576813b705f24dcc290
Andrés Felipe Barco Santa <andres@engflow.com>
2025-07-18 13:51:32 -0500

    NQ example: fix token usage in code & docs (#401)
    Towards linear/OND-1489

--- java/com/engflow/notificationqueue/Client.java
+++ java/com/engflow/notificationqueue/Client.java
@@ -84,9 +84,12 @@ class Client {
     }
     try {
       final Metadata header = new Metadata();
-      Metadata.Key<String> userKey =
-          Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER);
-      header.put(userKey, "Bearer " + clientOptions.getOption("token"));
+      Metadata.Key<String> methodKey =
+          Metadata.Key.of("x-engflow-auth-method", Metadata.ASCII_STRING_MARSHALLER);
+      header.put(methodKey, "jwt-v0");
+      Metadata.Key<String> tokenKey =
+          Metadata.Key.of("x-engflow-auth-token", Metadata.ASCII_STRING_MARSHALLER);
+      header.put(tokenKey, clientOptions.getOption("token"));
       pull(channel, clientOptions.getOption("queue_name"), header, forwardChannel);
     } finally {
       if (channel != null) {
