58f084181c9c405b16cdfd5b92c89d43b1fa0399
Luke Gehorsam <lgehorsam@engflow.com>
2025-07-28 12:55:58 -0400

    add platform-independent buck2 installation instructions

--- infra/setup-buck2.sh
+++ infra/setup-buck2.sh
@@ -10,3 +10,10 @@ unzstd buck2-x86_64-unknown-linux-musl.zst
 # Change its name and make it executable.
 mv buck2-x86_64-unknown-linux-musl buck2-exe
 chmod +x buck2-exe
+
+# Alternatively, for a platform-independent method that requires building from source:
+# Install the specific nightly version
+# rustup install nightly-2025-02-16
+
+# Use that specific version to install Buck2
+# cargo +nightly-2025-02-16 install --git https://github.com/facebook/buck2 --rev 201beb86106fecdc84e30260b0f1abb5bf576988 buck2
