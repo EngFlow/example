d99f352a1e8c6313d2f7edb3cb04509a5e77364d
John Cater <jcater@engflow.com>
2026-07-07 17:11:01 -0400

    Refactor example CI setup.
    This change:
    - Adds many more `build_test` targets to ensure non-tests will still be
      built and checked.
      - Some of these are not working and are currently disabled.
    - Splits each top-level package into a separate CI action so that it's
      easier to spot failures and ensure other tests continue
    - Adds a new tag-based way to disable tests:
      - `no-ci` will skip the test in CI entirely
      - `no-remote-ci` and `no-local-ci` will skip based on remote execution
      - `no-linux-ci` and `no-macos-ci` will skip based on the OS

--- infra/test-all.py
+++ infra/test-all.py
@@ -1,21 +1,70 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import subprocess
 import sys
 
-def main():
-    # All targets that can run in any environment, except ios examples.
-    targets = [
-        "//...",
-        # TODO: Add testing configuration for ios tests.
-        "-//ios/...",
-    ]
+ALL_PACKAGES = [
+    "cpp",
+    "csharp",
+    "docker",
+    "genrules",
+    "go",
+    "ios",
+    "java",
+    "kotlin",
+    "perl",
+    "scala",
+    "swift",
+    "typescript",
+]
 
-    for key in ("ARCH", "OPAL_RPC_CREDENTIALS", "OS", "REMOTE_EXECUTION"):
+EXTRA_FLAGS = {
+    "ios": ["--config=ios"],
+    "swift": ["--config=clang"],
+}
+
+
+def report_error(msg):
+    print(msg, file=sys.stderr)
+    sys.exit(1)
+
+
+def validate_env():
+    for key in ("ARCH", "OS", "REMOTE_EXECUTION", "OPAL_RPC_CREDENTIALS"):
         if not os.getenv(key):
-            sys.stderr.write(f"{key} not set\n")
-            sys.exit(1)
+            report_error(f"{key} not set")
+
+
+def determine_packages(packages, skip=[]):
+    return [p for p in packages if p not in skip]
+
+
+def find_tests(package):
+    os_name = os.getenv("OS")
+    remote = "remote" if os.getenv("REMOTE_EXECUTION") == "true" else "local"
+    # Remove no-ci tests and no-{os_name}-ci tests
+    query = f"""
+    let t = tests(//{package}/...) in
+    $t - 
+    attr(tags, no-ci, $t) - 
+    attr(tags, no-{os_name}-ci, $t) - 
+    attr(tags, no-{remote}-ci, $t)
+    """.strip()
+    print(f"Executing query to find test targets:\n{query}")
+    args = ["bazel", "query", "--output=label", "--", query]
+    result = subprocess.run(args, capture_output=True)
+    output = result.stdout.decode()
+    return [t for t in output.split("\n") if t]
+
+
+def run_tests(package):
+    targets = find_tests(package)
+    if not targets:
+        print(f"No targets found for {package}, skipping.")
+        return 0
+    print(f"Executing tests {targets}")
 
     os_arch = os.getenv("OS") + "_" + os.getenv("ARCH")
     flags = ["--config=ci"]
@@ -29,13 +78,62 @@ def main():
             "--config=opal_bes",
             "--config=opal_auth",
         ]
-    # The //docker/sysbox/... targets should only run in linux + remote
-    if os.getenv("REMOTE_EXECUTION") == "true" and os.getenv("OS") == "linux":
-        targets += ["//docker/sysbox/dind_test:check_docker",]
-    args = ["bazel", "test"] + flags + ["--"] + targets
 
-    result = subprocess.run(args)
-    sys.exit(result.returncode)
+    if package in EXTRA_FLAGS:
+        flags += EXTRA_FLAGS[package]
+
+    print(f"---------------\nTesting {package}...")
+    sys.stdout.flush()
+
+    args = ["bazel", "test"] + flags + ["--"] + targets
+    result = subprocess.run(args, check=False)
+    sys.stdout.flush()
+    sys.stderr.flush()
+    return result.returncode
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Runs example test suites", fromfile_prefix_chars="@"
+    )
+    parser.add_argument(
+        "--package",
+        required=False,
+        help="Package to test.",
+    )
+    parser.add_argument(
+        "--skip-packages",
+        required=False,
+        default=[],
+        type=lambda arg: arg.split(","),
+        help="Packages to skip testing.",
+    )
+    parser.add_argument(
+        "--validate-env",
+        action=argparse.BooleanOptionalAction,
+        help="Whether to validate environment variables",
+    )
+    opts = parser.parse_args()
+
+    if opts.validate_env:
+        validate_env()
+
+    packages = []
+    if opts.package:
+        packages = [opts.package]
+    else:
+        packages = determine_packages(ALL_PACKAGES, skip=opts.skip_packages)
+
+    # Make sure all packages are valid
+    for package in packages:
+        if package not in ALL_PACKAGES:
+            report_error(f"Unknown package {package}")
+
+    for package in packages:
+        returncode = run_tests(package)
+        # TODO: run all tests and then report any failures?
+        if returncode != 0:
+            sys.exit(returncode)
 
 
 if __name__ == "__main__":
